ff15d4e6b8289dfabe8a4cb40181ee7abfa8dae2
carlo-baron <carlobaron430@gmail.com>
2025-10-08 21:24:41 +0800

    fix: keep only one metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-};
-
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "Berry Catch",
